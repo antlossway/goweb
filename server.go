e54027c71985f3e3803d2183412e5e5e4658ffb5
antlossway <antlossway@gmail.com>
2018-07-23 21:05:35 +0200

    move wiki.go out

--- server.go
+++ server.go
@@ -3,18 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
-	"/Users/xqy/dev/go/web/login"
+	"fmt"
+//	"/Users/xqy/dev/go/web/login"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	//fmt.Fprintf(w, "Hi there, I love %s", r.URL.Path[1:])
-	user, ok1 := r.URL.Query().Get("user")
+	fmt.Fprintf(w, "Hi there, I love %s", r.URL.Path[1:])
+/*	user, ok1 := r.URL.Query().Get("user")
 	password, ok2 := r.URL.Query().Get("password")
 	if !ok1 or !ok2 {
 		w.WriteHeader(http.StatusNotFound)
 	} else {
 		login.checkuser(user, password)
 	}
+*/
 	
 }
 
